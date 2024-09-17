eea7134b75e858a50f30c4c9fbef345f0576f929
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 18:25:26 +0200

    task board

--- src/app/components/task-board/task-board.component.ts
+++ src/app/components/task-board/task-board.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './task-board.component.css'
 })
 export class TaskBoardComponent {
-
+  columns = [
+    {
+      name: 'To Do',
+      tasks: [
+        { title: 'Task 1', description: 'This is task 1' },
+        { title: 'Task 2', description: 'This is task 2' },
+      ],
+    },
+    {
+      name: 'Doing',
+      tasks: [
+        { title: 'Task 3', description: 'This is task 3' },
+        { title: 'Task 4', description: 'This is task 4' },
+      ],
+    },
+    {
+      name: 'Done',
+      tasks: [
+        { title: 'Task 5', description: 'This is task 5' },
+        { title: 'Task 6', description: 'This is task 6' },
+      ],
+    },
+  ];
 }
