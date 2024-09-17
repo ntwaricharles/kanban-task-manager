de02477d94e711e3204da3ef18eea83b5a187f4c
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 18:35:15 +0200

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'kanban-task-management';
+  isDarkMode = false;
+
+  ngOnInit() {
+    // Load dark mode preference
+    this.isDarkMode = localStorage.getItem('darkMode') === 'true';
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TaskBoardComponent } from './components/task-board/task-board.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    TaskBoardComponent
+    TaskBoardComponent,
+    TaskListComponent,
+    TaskCardComponent
   ],
   imports: [
     BrowserModule,
