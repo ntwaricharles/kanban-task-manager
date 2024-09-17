394b6db45b9820d484472d1112bdff224dccfc94
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 14:11:27 +0200

    mend
    navbar and sdebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TaskBoardComponent } from './components/task-board/task-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    TaskBoardComponent
   ],
   imports: [
     BrowserModule,
