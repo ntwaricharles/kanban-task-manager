78e452d1abf44216f1b5d3aa8d266cedc7c1091e
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 14:13:36 +0200

    app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { TaskBoardComponent } from './components/task-board/task-board.component';
 
 @NgModule({
