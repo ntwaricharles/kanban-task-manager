aeb7dc51770fca9450b25fb4dc310eeef2e2822c
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 14:14:27 +0200

    kanban task management

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#20212C',
+        'darker-blue': '#000112',
+        'light-purple': '#A8A4FF',
+        'dark-purple': '#635FC7',
+        'dark': '#2B2C37',
+        'grayish-dark': '#3E3F4E',
+        'grayish-blue': '#828FA3',
+        'light-grayish-blue': '#E4EBFA',
+        'lighter-grayish-blue': '#F4F7FD',
+        'white': '#FFFFFF',
+        'red': '#EA5555',
+        'light-red': '#FF9898',
+      },
+    },
+  },
+  plugins: [],
+}
