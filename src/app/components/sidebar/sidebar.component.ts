3cd024424e6bad39c6e98fdff2131cc163ce74bd
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 18:31:19 +0200

    sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  isSidebarHidden = false;
 
+  toggleSidebar() {
+    this.isSidebarHidden = !this.isSidebarHidden;
+  }
+
+  toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark');
+    localStorage.setItem(
+      'darkMode',
+      body.classList.contains('dark').toString()
+    );
+  }
+
+  ngOnInit() {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+    }
+  }
 }
+
