2207514456827bcb3e3f41985809897626134815
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-17 14:09:08 +0200

    navbar

---
+++ src/app/components/task-board/task-board.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-board',
+  templateUrl: './task-board.component.html',
+  styleUrl: './task-board.component.css'
+})
+export class TaskBoardComponent {
+
+}
